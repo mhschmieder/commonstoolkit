59118f1ef01fc98348a1fc25ce16c41a88c94dc7
Mark Schmieder <markhschmieder@gmail.com>
2021-11-08 01:12:57 -0500

    Added a lot of new helper methods for I/O and File stuff, especially ZIP

--- src/main/java/com/mhschmieder/commonstoolkit/io/FileUtilities.java
+++ src/main/java/com/mhschmieder/commonstoolkit/io/FileUtilities.java
@@ -31,9 +31,23 @@
 package com.mhschmieder.commonstoolkit.io;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.apache.commons.io.FilenameUtils;
+
+import com.mhschmieder.commonstoolkit.branding.ProductBranding;
 
 /**
  * {@code FileUtilities} is a static utilities class for common file
@@ -46,6 +60,27 @@ public final class FileUtilities {
      * The default constructor is disabled, as this is a static utilities class.
      */
     private FileUtilities() {}
+
+    // Get a package path from a jar-resident class instance, to use as a file
+    // prefix when supplying only the file name.
+    @SuppressWarnings("nls")
+    public static String getPackagePath( @SuppressWarnings("rawtypes") final Class classInstance ) {
+        final String packagePath = "/"
+                + classInstance.getClass().getPackage().getName().replaceAll( "\\.", "/" );
+        return packagePath;
+    }
+
+    // Generic method to get the "saved from" text for a document.
+    public static String getSavedFrom( final ProductBranding productBranding,
+                                       final Locale locale ) {
+        final StringBuilder savedFrom = new StringBuilder();
+        savedFrom.append( "Saved from " ); //$NON-NLS-1$
+        savedFrom.append( productBranding.productVersionProtected );
+        savedFrom.append( "; Locale = " ); //$NON-NLS-1$
+        savedFrom.append( locale.getDisplayName() );
+
+        return savedFrom.toString();
+    }
 
     /**
      * Return the extension portion of the file's name, in lower case.
@@ -73,6 +108,275 @@ public final class FileUtilities {
     public static String getExtension( final String filename ) {
         final String extension = FilenameUtils.getExtension( filename );
         return extension.toLowerCase( Locale.ENGLISH );
+    }
+
+    /**
+     * Return the file's name minus the extension portion.
+     * 
+     * @param file
+     *            The file whose extension should be stripped
+     * @return The original filename sans extension
+     */
+    public static String removeExtension( final File file ) {
+        if ( file == null ) {
+            return null;
+        }
+
+        final String filename = file.getName();
+        return removeExtension( filename );
+    }
+
+    /**
+     * Return the filename minus the extension portion.
+     * 
+     * @param filename
+     *            The filename whose extension should be stripped
+     * @return The original filename sans extension
+     */
+    public static String removeExtension( final String filename ) {
+        return FilenameUtils.removeExtension( filename );
+    }
+
+    public static StringBuilder getFileNameWithNewSuffix( final File file,
+                                                          final String fileSuffix ) {
+        try {
+            final String canonicalPath = file.getCanonicalPath();
+            if ( canonicalPath == null ) {
+                return null;
+            }
+
+            // Replace the old suffix with the required suffix.
+            final String newSuffixFileName = FilenameUtils.removeExtension( canonicalPath ) + '.'
+                    + fileSuffix;
+            final StringBuilder filenameBuffer = new StringBuilder( newSuffixFileName );
+            return filenameBuffer;
+        }
+        catch ( final Exception e ) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Move or rename a source file to a target file (system-specific).
+    public static boolean moveFile( final File sourceFile, final File targetFile ) {
+        // Do not move invalid or empty source files.
+        if ( ( sourceFile != null ) && ( sourceFile.length() > 0 ) ) {
+            // TODO: Verify that network drives work as the target location, as
+            // it is illegal to rename (vs. copy) a file from one system to
+            // another. Probably Java NIO2 deals with this for us.
+            try {
+                // NOTE: File status can change suddenly, so it is best to
+                // recheck whether a target file is writable before a move or
+                // rename operation. We always replace an existing file, and
+                // depend on the file chooser to alert the user of overwrites.
+                final Path sourcePath = sourceFile.toPath();
+                final Path targetPath = targetFile.toPath();
+                final boolean isTargetFile = Files.exists( targetPath, LinkOption.NOFOLLOW_LINKS )
+                        && !Files.notExists( targetPath, LinkOption.NOFOLLOW_LINKS );
+                if ( !isTargetFile ) {
+                    // If the target file doesn't exist, create it.
+                    Files.createFile( targetPath );
+                }
+
+                if ( !Files.isWritable( targetPath ) ) {
+                    return false;
+                }
+
+                // NOTE: If we also specify to copy the attributes, we get
+                // run-time exceptions on Windows 10 due to security issues.
+                Files.move( sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING );
+            }
+            catch ( final Exception e ) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // Move or rename a source file to a target file (system-specific).
+    public static boolean copyFile( final File sourceFile, final File targetFile ) {
+        // Do not copy invalid or empty source files.
+        if ( ( sourceFile != null ) && ( sourceFile.length() > 0 ) ) {
+            try {
+                // NOTE: File status can change suddenly, so it is best to
+                // recheck whether a target file is writable before a copy
+                // operation. We always replace an existing file, and depend on
+                // the file chooser to alert the user of overwrites.
+                final Path sourcePath = sourceFile.toPath();
+                final Path targetPath = targetFile.toPath();
+                if ( !Files.isWritable( targetPath ) ) {
+                    return false;
+                }
+
+                // TODO: Verify this doesn't throw exceptions, once this
+                // method is actually used again, as copying attributes is a
+                // problem when moving a file vs. simply copying it.
+                Files.copy( sourcePath,
+                            targetPath,
+                            StandardCopyOption.REPLACE_EXISTING,
+                            StandardCopyOption.COPY_ATTRIBUTES );
+            }
+            catch ( final Exception e ) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    // Get a time and version tagged file, given a fully specified path as
+    // candidate, alongside a client-supplied version string.
+    @SuppressWarnings("nls")
+    public static File getTimeAndVersionTaggedFile( final StringBuilder fileNameCandidate,
+                                                    final String version ) {
+        // Get the initial version tagged file name candidate.
+        final StringBuilder versionTaggedFileNameCandidate =
+                                                           getVersionTaggedFileName( fileNameCandidate,
+                                                                                     version );
+
+        // Add a time stamp that will allow for easy traceability of origin.
+        final String timeStamp =
+                               "_" + new SimpleDateFormat( "yyyyMMddHHmmss" ).format( new Date() );
+
+        // Append the modified revision string to the file name candidate.
+        final int fileSuffixIndex = fileNameCandidate.lastIndexOf( "." );
+        final StringBuilder timeAndVersionTaggedFileNameCandidate = ( fileSuffixIndex != -1 )
+            ? versionTaggedFileNameCandidate.insert( fileSuffixIndex, timeStamp )
+            : versionTaggedFileNameCandidate.append( timeStamp );
+        final File timeAndVersionTaggedFile = new File( timeAndVersionTaggedFileNameCandidate
+                .toString() );
+
+        return timeAndVersionTaggedFile;
+    }
+
+    // Get a unique version tagged file, given a fully specified path as
+    // candidate, alongside a client-supplied version string.
+    public static File getUniqueVersionTaggedFile( final StringBuilder fileNameCandidate,
+                                                   final String version ) {
+        // Get the initial version tagged file name candidate.
+        final StringBuilder versionTaggedFileNameCandidate =
+                                                           getVersionTaggedFileName( fileNameCandidate,
+                                                                                     version );
+
+        // Conditionally revision tag the version tagged file name.
+        final File uniqueFile = getUniqueRevisionTaggedFile( versionTaggedFileNameCandidate );
+
+        return uniqueFile;
+    }
+
+    // Get a unique revision tagged file, given a fully specified path as the
+    // file name candidate.
+    @SuppressWarnings("nls")
+    public static File getUniqueRevisionTaggedFile( final StringBuilder fileNameCandidate ) {
+        File uniqueFile = null;
+
+        try {
+            Path path = Paths.get( fileNameCandidate.toString() );
+            int fileRevision = 1;
+            while ( Files.isRegularFile( path, LinkOption.NOFOLLOW_LINKS ) ) {
+                // Append a recursively incremented revision number until we
+                // have a unique file name in the specified directory.
+                final String fileRevisionTag = "_r" + Integer.toString( fileRevision++ );
+
+                // Append the modified revision string to the file name.
+                final int fileSuffixIndex = fileNameCandidate.lastIndexOf( "." );
+                final StringBuilder revisionTaggedFileNameCandidate = ( fileSuffixIndex != -1 )
+                    ? fileNameCandidate.insert( fileSuffixIndex, fileRevisionTag )
+                    : fileNameCandidate.append( fileRevisionTag );
+
+                // Make a new path to check for uniqueness within the directory.
+                path = Paths.get( revisionTaggedFileNameCandidate.toString() );
+            }
+
+            uniqueFile = path.toFile();
+        }
+        catch ( final Exception e ) {
+            e.printStackTrace();
+        }
+
+        return uniqueFile;
+    }
+
+    // Get a version tagged file name, given a fully specified path as
+    // candidate, alongside a client-supplied version string.
+    public static StringBuilder getVersionTaggedFileName( final StringBuilder fileNameCandidate,
+                                                          final String version ) {
+        // Avoid throwing null pointer exceptions.
+        if ( fileNameCandidate == null ) {
+            return null;
+        }
+
+        // Make sure the supplied version string doesn't contain directory
+        // indirection markers; use dashes in place of dots for version string.
+        String versionTag = "_v" + version; //$NON-NLS-1$
+        versionTag = versionTag.replace( '.', '_' );
+
+        // Append the modified version string to the file name candidate.
+        final int fileSuffixIndex = fileNameCandidate.lastIndexOf( "." ); //$NON-NLS-1$
+        final StringBuilder versionTaggedFileNameCandidate = ( fileSuffixIndex != -1 )
+            ? fileNameCandidate.insert( fileSuffixIndex, versionTag )
+            : fileNameCandidate.append( versionTag );
+
+        return versionTaggedFileNameCandidate;
+    }
+
+    /**
+     * This method finds the first occurrence of a particular file type in a
+     * supplied ZIP file, and returns a ready-to-use ZIP Entry wrapped in an
+     * Optional so as to avoid null pointer exceptions.
+     *
+     * @param zipFile
+     *            The ZIP File to search for a specific File Type
+     * @param fileType
+     *            The simple file extension used to search for the File Type
+     * @return A ZIP Entry corresponding to the first match, or null if not
+     *         present, but wrapped as an Optional for code safety
+     */
+    public static Optional< ? extends ZipEntry > findFileTypeInZip( final ZipFile zipFile,
+                                                                    final String fileType ) {
+        // Each file access has to start a new stream due to auto-close.
+        final Stream< ? extends ZipEntry > zipStream = zipFile.stream();
+        final Predicate< ZipEntry > isCorrectFileType = zipEntry -> FilenameUtils
+                .isExtension( zipEntry.getName().toLowerCase( Locale.ENGLISH ), fileType );
+
+        final Predicate< ZipEntry > isFile = zipEntry -> !zipEntry.isDirectory();
+        final Optional< ? extends ZipEntry > optionalZipEntry = zipStream
+                .filter( isFile.and( isCorrectFileType ) ).findFirst();
+
+        return optionalZipEntry;
+    }
+
+    /**
+     * This method finds an occurrence of a particular file name in a
+     * supplied ZIP file, and returns a ready-to-use ZIP Entry wrapped in an
+     * Optional so as to avoid null pointer exceptions.
+     *
+     * @param zipFile
+     *            The ZIP File to search for a specific File Name
+     * @param fileName
+     *            The simple file name to search for, sans path
+     * @return A ZIP Entry corresponding to a file match, or null if not
+     *         present, but wrapped as an Optional for code safety
+     */
+    public static Optional< ? extends ZipEntry > findFileNameInZip( final ZipFile zipFile,
+                                                                    final String fileName ) {
+        // Each file access has to start a new stream due to auto-close.
+        final Stream< ? extends ZipEntry > zipStream = zipFile.stream();
+        final Predicate< ZipEntry > isCorrectFileName = zipEntry -> zipEntry.getName()
+                .equals( fileName );
+
+        final Predicate< ZipEntry > isFile = zipEntry -> !zipEntry.isDirectory();
+        final Optional< ? extends ZipEntry > optionalZipEntry = zipStream
+                .filter( isFile.and( isCorrectFileName ) ).findFirst();
+
+        return optionalZipEntry;
     }
 
 }
--- src/main/java/com/mhschmieder/commonstoolkit/io/IoUtilities.java
+++ src/main/java/com/mhschmieder/commonstoolkit/io/IoUtilities.java
@@ -31,20 +31,30 @@
 package com.mhschmieder.commonstoolkit.io;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.UnknownServiceException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.Charsets;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.input.SwappedDataInputStream;
+
+import com.mhschmieder.commonstoolkit.branding.ProductBranding;
 
 /**
  * {@code IoUtilities} is a static utilities class for common I/O functionality
@@ -55,7 +65,7 @@ import org.apache.commons.io.input.SwappedDataInputStream;
  */
 public final class IoUtilities {
 
-    // :NOTE: We might need to switch this to UTF-8 encoding.
+    // NOTE: We might need to switch this to UTF-8 encoding.
     @SuppressWarnings("nls") public static final String LATIN_1 = "ISO-8859-1";
 
     /**
@@ -96,7 +106,7 @@ public final class IoUtilities {
 
     // Generalized method to load a float array from a Little Endian Input
     // Stream to a double array with possible offset and default-fill.
-    // :NOTE: The data array must be pre-allocated, but is filled here.
+    // NOTE: The data array must be pre-allocated, but is filled here.
     public static boolean loadIntoDoubleArray( final SwappedDataInputStream inputStream,
                                                final double[] data,
                                                final int dataOffset,
@@ -256,6 +266,136 @@ public final class IoUtilities {
             ioe.printStackTrace();
             return null;
         }
+    }
+
+    // Function to import a remote stream to a byte array cache, usually sent
+    // as a ZIP stream and especially necessary to save as raw bytes so as to
+    // preserve all of the ZIP structure details for later retrieval.
+    //
+    // The returned string is meant as a status message for a progress bar.
+    public static byte[] saveRemoteStreamToByteArray( final HttpURLConnection httpURLConnection,
+                                                      final StringBuilder statusMessage ) {
+        // Attempt to load the data stream from the server.
+        //
+        // In order to minimize the dependency on the server connection, and
+        // also to compensate for servlet response streams being consumed in a
+        // "read once" manner, we immediately perform a byte array copy of the
+        // entire stream into local memory.
+        //
+        // Reading data directly from a URL can be very expensive, as each read
+        // access reaches across the internet or to a local system cache that is
+        // not in local program address space.
+        //
+        // This code streams the data from the remote file object into a local
+        // byte array stream, preserving byte order so that floating-point
+        // interpretation (Little Endian/Big Endian) can be deferred to a
+        // downstream filter.
+        try ( final InputStream servletResponseStream = httpURLConnection.getInputStream() ) {
+            // Immediately cache the server response to a byte array in local
+            // memory, for efficient and flexible reuse, and due to the a
+            // frequent need to cycle through this stream twice (as a result of
+            // order-dependent processing).
+            final byte[] byteArray = saveStreamToByteArray( servletResponseStream, statusMessage );
+            return byteArray;
+        }
+        catch ( final NullPointerException npe ) {
+            npe.printStackTrace();
+            statusMessage.append( "Server Communication Error: Invalid Response Stream Handle." ); //$NON-NLS-1$
+        }
+        catch ( final UnknownServiceException use ) {
+            use.printStackTrace();
+            statusMessage
+                    .append( "Server Protocol Error: Server Does Not Support Response Streams." ); //$NON-NLS-1$
+        }
+        catch ( final SocketTimeoutException ste ) {
+            ste.printStackTrace();
+            statusMessage.append( "Server Communication Error: Socket Read Timeout." ); //$NON-NLS-1$
+        }
+        catch ( final IOException ioe ) {
+            ioe.printStackTrace();
+            statusMessage.append( "Server Communication Error: Response Stream I/O Problems." ); //$NON-NLS-1$
+        }
+
+        return null;
+    }
+
+    // Function to import a stream to a byte array cache.
+    //
+    // The returned string is meant as a status message for a progress bar.
+    public static byte[] saveStreamToByteArray( final InputStream inputStream,
+                                                final StringBuilder statusMessage )
+            throws IOException {
+        // Attempt to load the ZIP data stream into a byte array.
+        //
+        // This code streams the data from the remote file object into a local
+        // byte array stream, preserving byte order so that floating-point
+        // interpretation (Little Endian/Big Endian) can be deferred to a
+        // downstream filter.
+        try ( final InputStream bufferedInputStream =
+                                                    IOUtils.toBufferedInputStream( inputStream ) ) {
+            final byte[] byteArray = IOUtils.toByteArray( bufferedInputStream );
+            return byteArray;
+        }
+    }
+
+    // Function to export a byte array to a ZIP file with product signature.
+    public static FileStatus saveByteArrayToZip( final byte[] byteArray,
+                                                 final ProductBranding productBranding,
+                                                 final Locale locale,
+                                                 final ZipOutputStream zipOutputStream ) {
+        try {
+            // Add a header so that WINZIP and other tools can easily identify
+            // this as a ZIP file produced from MAPP (vs. other apps).
+            prepareZipForWrite( zipOutputStream, productBranding, locale );
+
+            // Chain a ZipInputStream to a ByteArrayInputStream to the byte
+            // array, to inflate the ZIP entries.
+            try ( final ByteArrayInputStream byteArrayInputStream =
+                                                                  new ByteArrayInputStream( byteArray );
+                    final ZipInputStream zipInputStream =
+                                                        new ZipInputStream( byteArrayInputStream ) ) {
+                ZipEntry zipEntry = null;
+                while ( ( zipEntry = zipInputStream.getNextEntry() ) != null ) {
+                    // Echo the current ZIP entry to the saved ZIP file (this
+                    // should also preserve the original server time/date
+                    // stamp).
+                    zipOutputStream.putNextEntry( zipEntry );
+
+                    // Copy the current ZIP entry from the byte array directly
+                    // to the new ZIP output file.
+                    // NOTE: We don't care about the returned data size; if it
+                    // was "-1", it simply means it was too large to fit in an
+                    // int.
+                    IOUtils.copy( zipInputStream, zipOutputStream );
+
+                    // Close the current ZIP input entry to prepare to read the
+                    // next entry.
+                    zipInputStream.closeEntry();
+
+                    // Close the current ZIP output entry (copy) to prevent
+                    // further modification.
+                    zipOutputStream.closeEntry();
+                }
+            }
+        }
+        catch ( final Exception e ) {
+            e.printStackTrace();
+            return FileStatus.WRITE_ERROR;
+        }
+
+        return FileStatus.SAVED;
+    }
+
+    // Utility to prepare a ZIP output stream for write operations (i.e. adding
+    // entries). This means adding a comment, setting the deflation method and
+    // the compression level.
+    public static void prepareZipForWrite( final ZipOutputStream zipOutputStream,
+                                           final ProductBranding productBranding,
+                                           final Locale locale ) {
+        final String savedFrom = FileUtilities.getSavedFrom( productBranding, locale );
+        zipOutputStream.setComment( savedFrom );
+        zipOutputStream.setMethod( ZipOutputStream.DEFLATED );
+        zipOutputStream.setLevel( 9 );
     }
 
 }
