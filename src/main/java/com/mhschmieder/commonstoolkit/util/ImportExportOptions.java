262cb79f915234d68001f72bd619cce0f10ff56f
Mark Schmieder <markhschmieder@gmail.com>
2022-03-16 00:25:05 -0400

    Create ImportExportOptions.java
    This interface is used a bunch of places and has no dependencies, so the best place for it is in the utilities area of the lowest level commons toolkit

---
+++ src/main/java/com/mhschmieder/commonstoolkit/util/ImportExportOptions.java
@@ -0,0 +1,52 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2020, 2022 Mark Schmieder
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ * This file is part of the CommonsToolkit Library
+ *
+ * You should have received a copy of the MIT License along with the
+ * CommonsToolkit Library. If not, see <https://opensource.org/licenses/MIT>.
+ *
+ * Project: https://github.com/mhschmieder/commonstoolkit
+ */
+package com.mhschmieder.commonstoolkit.util;
+
+/**
+ * This interface assigns the minimal set of methods needed on all
+ * Import/Export actions, to enforce consistency of naming and coverage.
+ * <p>
+ * This is specifically meant to be implemented by data models that pair
+ * with GUI option panes used in import and export actions.
+ */
+public interface ImportExportOptions {
+
+    default void clearAll() {
+        setSelected( false );
+    }
+
+    void reset();
+
+    void setAll();
+
+    void setSelected( final boolean selected );
+
+}
