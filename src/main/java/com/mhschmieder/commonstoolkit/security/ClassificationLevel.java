880ca76d334d5659df0028e41d5d70f0d57cf2b4
Mark Schmieder <markhschmieder@gmail.com>
2025-01-11 12:13:21 -0500

    Update ClassificationLevel.java
    Forgot to include CUI. While I was at it, I converted this enum to implement the new boilerplate-reduction interfaces for labels and abbreviations.

--- src/main/java/com/mhschmieder/commonstoolkit/security/ClassificationLevel.java
+++ src/main/java/com/mhschmieder/commonstoolkit/security/ClassificationLevel.java
@@ -1,7 +1,7 @@
 /**
  * MIT License
  *
- * Copyright (c) 2020, 2024 Mark Schmieder
+ * Copyright (c) 2020, 2025 Mark Schmieder
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -30,93 +30,74 @@
  */
 package com.mhschmieder.commonstoolkit.security;
 
-public enum ClassificationLevel {
-    UNCLASSIFIED, CONFIDENTIAL, SECRET, TOP_SECRET;
+import com.mhschmieder.commonstoolkit.lang.Abbreviated;
+import com.mhschmieder.commonstoolkit.lang.EnumUtilities;
+import com.mhschmieder.commonstoolkit.lang.Labeled;
+
+/**
+ * An enumeration of current US government classification levels, which were
+ * modified starting around 2017 or thereabouts.
+ * <p>
+ * NOTE: Until hearing otherwise, the verbose "Controlled Unclassified
+ *  Information" classification level is only presented by its "CUI" acronym.
+ * <p>
+ * TODO: Carefully vet the current abbreviations and re-review CUI for any
+ *  preferred unabbreviated forms, as well as case-sensitivity of all terms.
+ */
+public enum ClassificationLevel implements Labeled< ClassificationLevel >, 
+        Abbreviated< ClassificationLevel > {
+    UNCLASSIFIED( "Unclassified", "U" ), 
+    CUI( "CUI", "CUI" ), 
+    CONFIDENTIAL( "Confidential", "C" ), 
+    SECRET( "Secret", "S" ), 
+    TOP_SECRET( "Top Secret", "TS" );
+    
+    private String label;
+    private String abbreviation;
+    
+    ClassificationLevel( final String pLabel,
+                         final String pAbbreviation ) {
+        label = pLabel;
+        abbreviation = pAbbreviation;
+    }
+
+    @Override 
+    public final String label() {
+        return label;
+    }
+
+    @Override
+    public ClassificationLevel valueOfLabel( final String text ) {
+        return ( ClassificationLevel ) EnumUtilities.getLabeledEnumFromLabel( 
+            text, values() );
+    }
+
+    @Override 
+    public final String abbreviation() {
+        return abbreviation;
+    }
+
+    @Override
+    public ClassificationLevel valueOfAbbreviation( final String abbreviatedText ) {
+        return ( ClassificationLevel ) EnumUtilities.getAbbreviatedEnumFromAbbreviation( 
+            abbreviatedText, values() );
+    }
 
     public static ClassificationLevel defaultValue() {
         return UNCLASSIFIED;
     }
 
-    @SuppressWarnings("nls")
-    public static ClassificationLevel fromAbbreviatedString( final String clientTypeAbbreviatedString ) {
-        if ( "unclassified".equalsIgnoreCase( clientTypeAbbreviatedString ) ) {
-            return UNCLASSIFIED;
-        }
-
-        if ( "confidential".equalsIgnoreCase( clientTypeAbbreviatedString ) ) {
-            return CONFIDENTIAL;
-        }
-
-        if ( "secret".equalsIgnoreCase( clientTypeAbbreviatedString ) ) {
-            return SECRET;
-        }
-
-        if ( ( "top secret".equalsIgnoreCase( clientTypeAbbreviatedString )
-                || "top_secret".equalsIgnoreCase( clientTypeAbbreviatedString ) ) ) {
-            return TOP_SECRET;
-        }
-
-        return defaultValue();
-    }
-
     @Override
     public final String toString() {
         // NOTE: As of Java 6, enums include the underscore in their string
-        // representation, which is a problem for backward-compatibility with
-        // XML parsers. Thus, we need to strip the underscores and replace them
-        // with spaces, to behave like Java 5.
+        //  representation, which is a problem for backward-compatibility with
+        //  XML parsers. Thus, we need to strip the underscores and replace
+        //  them with spaces, to behave like Java 5, for legacy file handling.
+        // TODO: Determine whether this is still necessary from Java 8 onwards,
+        //  as well as whether it should be used for newer enums like this one
+        //  and whether current default string conversion called by Java core
+        //  could cause downstream problems if not "corrected" in this override.
         final String value = super.toString();
-        return value.replaceAll( "_", " " ); //$NON-NLS-1$ //$NON-NLS-2$
+        return value.replaceAll( "_", " " );
     }
-
-    public final String toAbbreviatedString() {
-        String abbreviatedString = null;
-        
-        switch ( this ) {
-        case UNCLASSIFIED:
-            abbreviatedString = "unclassified"; //$NON-NLS-1$
-            break;
-        case CONFIDENTIAL:
-            abbreviatedString = "confidential"; //$NON-NLS-1$
-            break;
-        case SECRET:
-            abbreviatedString = "secret"; //$NON-NLS-1$
-            break;
-        case TOP_SECRET:
-            abbreviatedString = "top secret"; //$NON-NLS-1$
-            break;
-        default:
-            final String errMessage = "Unexpected " //$NON-NLS-1$
-                    + this.getClass().getSimpleName() + " " + this; //$NON-NLS-1$
-            throw new IllegalArgumentException( errMessage );
-        }
-        
-        return abbreviatedString;
-    }
-
-    public final String toPresentationString() {
-        String presentationString = null;
-        
-        switch ( this ) {
-        case UNCLASSIFIED:
-            presentationString = "Unclassified"; //$NON-NLS-1$
-            break;
-        case CONFIDENTIAL:
-            presentationString = "Confidential"; //$NON-NLS-1$
-            break;
-        case SECRET:
-            presentationString = "Secret"; //$NON-NLS-1$
-            break;
-        case TOP_SECRET:
-            presentationString = "Top Secret"; //$NON-NLS-1$
-            break;
-        default:
-            final String errMessage = "Unexpected " //$NON-NLS-1$
-                    + this.getClass().getSimpleName() + " " + this; //$NON-NLS-1$
-            throw new IllegalArgumentException( errMessage );
-        }
-        
-        return presentationString;
-    }
-
 }
