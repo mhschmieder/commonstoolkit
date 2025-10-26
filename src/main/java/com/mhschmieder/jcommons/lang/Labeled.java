b55c093cc51d96be9a02db0765a100ea092f0544
Mark Schmieder <markhschmieder@gmail.com>
2025-10-26 01:07:57 -0400

    added some beneficial logic-based enums and associated GUI, and fixed an incorrectly named package hierarchy

--- src/main/java/com/mhschmieder/jcommons/lang/Labeled.java
+++ src/main/java/com/mhschmieder/jcommons/lang/Labeled.java
@@ -1,7 +1,7 @@
-/**
+/*
  * MIT License
  *
- * Copyright (c) 2023, 2025 Mark Schmieder
+ * Copyright (c) 2023, 2025, Mark Schmieder. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,10 +21,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *
- * This file is part of the JCommons Library
+ * This file is part of the jcommons Library
  *
- * You should have received a copy of the MIT License along with the
- * JCommons Library. If not, see <https://opensource.org/licenses/MIT>.
+ * You should have received a copy of the MIT License along with the jcommons
+ * Library. If not, see <https://opensource.org/licenses/MIT>.
  *
  * Project: https://github.com/mhschmieder/jcommons
  */
